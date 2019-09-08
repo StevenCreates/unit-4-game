632e78092c67bd8904209a28cc0d6b2a5c4cf7a1
Steven Hofheins <stevenisinsane@gmail.com>
2019-09-08 13:43:45 -0600

    updated frontent, added some psuedo code and game images

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// variables 
+
+// code that unhides enemies until legend is selected and hides other legends
+
+// code that adds class to hides other enemies
+
+// attack button that when clicked will do .random to determine what damage was done
+
+// 
