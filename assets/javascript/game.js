2a5a3aba28b26b462f554e77b98193d9a8b14fae
Steven Hofheins <stevenisinsane@gmail.com>
2019-09-09 13:32:50 -0600

    added more functionality to selections

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,77 +1,79 @@
-$(document).ready(function (){
+$(document).ready(function () {
 
-// variables 
-var attack = [];
-var selection = null;
-var currentHp = [];
-var currentLegend = null;
-var currentEnemy = null;
+    // variables 
+    var attack = [];
+    var selection = null;
+    var currentHp = [];
+    var currentLegend = null;
+    var currentEnemy = null;
 
-//establish characters stats
+    //establish characters stats
 
-var pathfinder = {
+    var pathfinder = {
         id: 0,
         name: "pathfinder",
         hp: 200,
         attack: 18,
     }
 
-var  wraith = {
+    var wraith = {
         id: 1,
         name: "wraith",
         hp: 150,
         attack: 15,
     }
 
-var octane = {
+    var octane = {
         id: 2,
         name: "octane",
         hp: 100,
         attack: 22,
     }
 
-var  wattson = {
+    var wattson = {
         id: 3,
         name: "wattson",
         hp: 175,
         attack: 20,
     }
-    
-
-//Unhides enemies when legend is selected.
-$(".legend").click(function (e) {
-    if ($(this).hasClass("legend")) {
-        currentLegend = $(this).val();
-        $(this).addClass("youSelected")
-    }
-
-    if ($('.legend')) {
-        $('.legend:not(.youSelected').addClass("hide")
-    }
 
 
+    //Adds class to selected legend
+    $(".legend").click(function (e) {
+        if ($(this).hasClass("legend")) {
+            currentLegend = $(this).val();
+            $(this).addClass("youSelected");
+            //   $(".youSelected").add("div");
+        }
+        //Adds class to legends not selected
+        if ($('.legend')) {
+            $('.legend:not(.youSelected').addClass("hide")
+        }
+
+        $('.enemies').addClass("show")
+        console.log(currentLegend)
+    })
+
+    //Grabs value of current legend
+    $(".fighter").click(function (e) {
+        if ($(this).hasClass("enemies")) {
+            currentEnemy = $(this).val();
+            $(this).addClass("enemySelected")
+        }
+        if ($('.enemies')) {
+            $('.enemies:not(.enemySelected').addClass("hideenemy")
+        }
+        $('.attackbut').addClass("show")
+        console.log(currentEnemy)
+    })
 
 
-    $('.enemies').addClass("show")
-    console.log(currentLegend)
-})
-
-//when enemie gets selected functionality 
-$(".fighter").click(function (e) {
-    if ($(this).hasClass("enemies")) {
-        currentEnemy = $(this).val();
-    }
-    $('.attackbut').addClass("show")
-    console.log(currentEnemy)
-})
-
-
-// attack button that when clicked will do .random to determine what damage was done
+    // attack button that when clicked will do .random to determine what damage was done
 
 
 
 
 
-//end doc ready function
+    //end doc ready function
 })
 
